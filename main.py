c84c33b328f0ba955cd23c8940ec6a0f6fd58236
Ivan Perlov <56824839+Torusaynim@users.noreply.github.com>
2021-04-04 18:57:01 +0300

    First commit
    Not much but it's something
    
    Co-Authored-By: Шамхал Шахгусейнов <56553437+shamhal3228@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import discord
+import random
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='>')
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+
+@bot.event
+async def on_message_delete(message):
+    channel = bot.get_channel(827903072667041802)
+    await channel.send("Deleted message: "+message.content+"\nAuthor: "+str(message.author))
+    #добавить время удаленного сообщения
+
+
+@bot.event
+async def on_message(message):
+    if message.content == 'vitali4': #сделать через список
+        await message.delete()
+    await bot.process_commands(message)
+
+@bot.command()
+async def kick(ctx, user: discord.Member):
+    await ctx.guild.kick(user)
+
+
+bot.run(TOKEN)
